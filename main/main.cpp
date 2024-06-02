d88a2dc3162be5798ae42133d8da6c224019156f
Maksim Shishkov <patiford@mail.ru>
2024-06-02 12:02:22 +0300

    .gitignore added

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,12 @@
 #include <mainwindow/mainwindow.hpp>
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
-    
+
     return a.exec();
 }
