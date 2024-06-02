b67b8370e76dde68c5673a60eafee2e5a8f54c64
Maksim Shishkov <144558164+notfoun9@users.noreply.github.com>
2024-06-02 11:58:35 +0300

    Delete build directory

--- build/CMakeFiles/simple_snake_qt_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/simple_snake_qt_autogen"
-  "simple_snake_qt_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/simple_snake_qt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
